22f9215a0b9f4f0e8f8534b8f875fd78912afd4b
MarcHaller <tahafidaly.abonnement@gmail.com>
2025-05-23 16:56:12 +0200

    version avec print enlever

--- Backend.py
+++ Backend.py
@@ -14,10 +14,10 @@ def encode_image(image_path):
         with open(image_path, "rb") as image_file:
             return base64.b64encode(image_file.read()).decode('utf-8')
     except FileNotFoundError:
-        print(f"Error: The file {image_path} was not found.")
+        
         return None
     except Exception as e:  # Added general exception handling
-        print(f"Error: {e}")
+        
         return None
 
 
@@ -62,5 +62,5 @@ def describe_image(image_path):
 
 
 image_path = "Image/fatoulafolle.jpeg"
-print(describe_image(image_path))
+
 
